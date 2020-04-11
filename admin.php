05a6bc16708ccc5c7e309b2447d3e80df28e1da6
Saul Baizman <saul.baizman@massart.edu>
2020-04-11 00:21:45 -0400

    Added admin.php stub.

---
+++ admin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package Baizman Design Standard Library
+ * @version 0.1
+ */
+
+/******************************************************************************/
+
+/*
+ * User-configurable settings and admin page functions.
+ */
