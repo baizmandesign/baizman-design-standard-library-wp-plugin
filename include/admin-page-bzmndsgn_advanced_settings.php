770ed457faa0c8e773a1659c781403155ef8b271
Saul Baizman <saul@baizmandesign.com>
2020-05-28 13:42:41 -0400

    Updated nonce names.

--- include/admin-page-bzmndsgn_advanced_settings.php
+++ include/admin-page-bzmndsgn_advanced_settings.php
@@ -9,16 +9,16 @@ function bzmndsgn_advanced_settings () {
 	<h4>Reserialize data</h4>
 	<form method="post" action="<?php echo admin_url('admin-post.php') ; ?>">
 		<p>If a new plugin setting has been added or removed, click the button below to synchronize the database.</p>
-		<input type="hidden" name="action" value="cas_reserialize_data">
-		<?php wp_nonce_field ( 'cas_reserialize_data' ); ?>
+		<input type="hidden" name="action" value="bzmndsgn_reserialize_data">
+		<?php wp_nonce_field ( 'bzmndsgn_reserialize_data' ); ?>
 		<?php submit_button( 'Reserialize configuration data', 'small', 'reserialize', $wrap = false ) ;?>
 	</form>
 	<h4>Reinitialize default data</h4>
 	<form method="post" action="<?php echo admin_url('admin-post.php') ; ?>">
 		<p>Reset the saved plugin data to the default values.</p>
 		<p><strong>Note: this will erase any local customizations! Use with caution.</strong></p>
-		<input type="hidden" name="action" value="cas_reinitialize_default_data">
-		<?php wp_nonce_field ( 'cas_reinitialize_default_data' ); ?>
+		<input type="hidden" name="action" value="bzmndsgn_reinitialize_default_data">
+		<?php wp_nonce_field ( 'bzmndsgn_reinitialize_default_data' ); ?>
 		<?php submit_button( 'Reinitialize default configuration', 'small', 'reserialize', $wrap = false ) ;?>
 	</form>
 
