100140e3eef104357a532b505ccebf9de79fc741
Saul Baizman <saul@baizmandesign.com>
2020-06-04 22:13:57 -0400

    Renamed several variables in class.text-input.php.

--- class/class.text-input.php
+++ class/class.text-input.php
@@ -5,30 +5,31 @@
 
 class text_input extends field {
 
-	// something for data validation?
-	// or length
+	// add data validation? for example, for length?
+	/**
+	 * @var
+	 */
 	private $field_placeholder ;
-
 
 	/**
 	 * text_input constructor.
 	 *
-	 * @param $field_label
-	 * @param $field_input_name
-	 * @param $placeholder
-	 * @param $default_value
+	 * @param $text_field_label
+	 * @param $text_field_input_name
+	 * @param $text_field_placeholder
+	 * @param $text_field_default_value
 	 */
-	public function __construct( $field_label, $field_input_name, $placeholder, $default_value ) {
-		parent::__construct( $field_label, $field_input_name );
-		$this->set_field_placeholder( $placeholder ) ;
-		$this->set_field_default_value ( $default_value ) ;
+	public function __construct( $text_field_label, $text_field_input_name, $text_field_placeholder, $text_field_default_value ) {
+		parent::__construct( $text_field_label, $text_field_input_name );
+		$this->set_field_placeholder( $text_field_placeholder ) ;
+		$this->set_field_default_value ( $text_field_default_value ) ;
 		$this->set_field_type( 'input' );
 	}
 
 	/**
 	 * @return mixed
 	 */
-	public function get_field_placeholder() {
+	public function get_text_field_placeholder() {
 		return $this->field_placeholder;
 	}
 
@@ -39,23 +40,26 @@ class text_input extends field {
 		$this->field_placeholder = $field_placeholder;
 	}
 
+	/**
+	 * @return mixed|void
+	 */
 	public function print_form_field ( ) {
 
 		printf ('<tr valign="top">') ;
 
 		printf (
-			'<th scope="row">%s:</th>',
+			'<th scope="row">%1$s:</th>',
 			$this->get_field_label( )
 		) ;
 		printf (
-			'<td><input type="text" name="%s" placeholder="%s" value="%s" size="50">',
+			'<td><input type="text" name="%1$s" placeholder="%2$s" value="%3$s" size="50">',
 			esc_attr ( $this->get_field_input_name() ),
-			esc_attr ( $this->get_field_placeholder() ),
+			esc_attr ( $this->get_text_field_placeholder() ),
 			esc_attr ( $this->get_field_default_value() )
 		);
 		if ( $this->get_help_text() ) {
 			printf (
-				'<br><small>%s</small>',
+				'<br><small>%1$s</small>',
 				$this->get_help_text()
 			) ;
 		}
