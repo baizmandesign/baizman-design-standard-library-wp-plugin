817e0ab2b35a12ad02ace692b68a911b4f0d6b06
Saul Baizman <saul@baizmandesign.com>
2020-06-05 21:43:07 -0400

    Set the Google Analytics ID dynamically.

--- include/functions-google-analytics.php
+++ include/functions-google-analytics.php
@@ -5,19 +5,18 @@ if ( ! function_exists ( 'bzmndsgn_enqueue_google_analytics' ) ):
      * Add Google Analytics tracking code to global header.
      */
     function bzmndsgn_enqueue_google_analytics ( ) {
-//	global $bzmndsgn_config_options ;
 
-        $bzmndsgn_config_options['google_analytics_id'] = 'UA-77345635-1' ;
+	    $bzmndsgn_config_options_database = get_option ( BZMNDSGN_CONFIG_OPTIONS );
 
-        if ( $bzmndsgn_config_options['google_analytics_id'] ) {
+	    if ( $bzmndsgn_config_options_database['google_analytics_id'] ) {
             ?>
             <!-- Global site tag (gtag.js) - Google Analytics -->
-            <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $bzmndsgn_config_options['google_analytics_id'] ; ?>"></script>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $bzmndsgn_config_options_database['google_analytics_id'] ; ?>"></script>
             <script>
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '<?php echo $bzmndsgn_config_options['google_analytics_id'] ; ?>');
+                gtag('config', '<?php echo $bzmndsgn_config_options_database['google_analytics_id'] ; ?>');
             </script>
             <?php
         }
