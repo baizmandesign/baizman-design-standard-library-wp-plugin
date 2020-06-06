d7408980f24e18e318eeea05ecd03595b94adfda
Saul Baizman <saul@baizmandesign.com>
2020-06-06 05:48:23 -0400

    Added hidden field class.

---
+++ class/class.hidden.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Hidden field class.
+ * @package Baizman Design Standard Library
+ * @version 0.1
+ */
+
+class hidden extends field {
+
+	/**
+	 * hidden field constructor.
+	 *
+	 * @param $hidden_field_input_name
+	 * @param $hidden_field_default_value
+	 */
+	public function __construct( $hidden_field_input_name, $hidden_field_default_value ) {
+		parent::__construct( '', $hidden_field_input_name );
+		$this->set_field_default_value ( $hidden_field_default_value ) ;
+		$this->set_field_type( 'hidden' );
+	}
+
+	/**
+	 * Print hidden field.
+	 * @return mixed|void
+	 */
+	public function print_form_field() {
+		printf (
+			'<input type="%1$s" name="%2$s" value="%3$s">',
+			$this->get_field_type(),
+			esc_attr ( $this->get_field_input_name() ),
+			esc_attr ( $this->get_field_default_value() )
+		) ;
+	}
+}
