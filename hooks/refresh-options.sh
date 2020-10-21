434cc677323c4089c0bf521cfb02f4f0a67b7a20
Saul Baizman <saul@baizmandesign.com>
2020-10-21 13:01:17 -0400

    Fixed typo.

--- hooks/refresh-options.sh
+++ hooks/refresh-options.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This scripts refreshes the database options to add new options and remove old ones.
+# This script refreshes the database options to add new options and remove old ones.
 # install in $GITDIR/hooks:
 # ln -s hooks/refresh-options.sh $GITDIR/hooks/post-merge
 
