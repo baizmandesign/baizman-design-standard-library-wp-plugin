c204d61b7909070f4b06e6156c292b3a5045d1a6
Saul Baizman <saul@baizmandesign.com>
2023-03-30 01:46:22 -0400

    Over-ride production update server on localhost.

--- class/class.updater.php
+++ class/class.updater.php
@@ -11,13 +11,18 @@ namespace baizman_design ;
 
 class updater {
 
-	public static string $update_domain = 'wp.baizmandesign.com' ;
+	public static string $production_update_domain = 'wp.baizmandesign.com' ;
+	public static string $localhost_update_domain = 'localhost' ;
+	public static int $localhost_update_port = 8000 ;
+
 	public static function add_filter () {
-		add_filter('update_plugins_'.self::$update_domain, [ __CLASS__,'check_for_updates' ], 10, 3);
+		// don't change the filter name for localhost over-ride
+		add_filter('update_plugins_'.self::$production_update_domain, [ __CLASS__,'check_for_updates' ], 10, 3);
 		add_filter( 'plugins_api', [ __CLASS__, 'plugin_modal_info' ], 20, 3);
 	}
 
 	public static function check_for_updates( $update, $plugin_data, $plugin_file ){
+
 		static $response = false;
 
 		if ( empty( $plugin_data['UpdateURI'] ) || ! empty( $update ) ) {
@@ -25,6 +30,18 @@ class updater {
 		}
 
 		if ( $response === false ) {
+
+			// over-ride production hostname
+			if ( wp_get_environment_type() == 'local' ) {
+				$update_uri = str_replace (
+					'https://'. self::$production_update_domain,
+					'http://' . self::$localhost_update_domain.':'. self::$localhost_update_port,
+					$plugin_data['UpdateURI'] ) ;
+			}
+			else {
+				$update_uri = $plugin_data['UpdateURI'] ;
+			}
+
 			// https://rudrastyh.com/wordpress/check-license-key-in-plugin-updates.html
 			// to quell wp cli warning about an undefined array key
 			$referrer_domain = $_SERVER['HTTP_HOST'] ?? '[unknown domain]' ;
@@ -33,12 +50,13 @@ class updater {
 					'asset' => urlencode (preferences::$plugin_slug),
 					'referrer_domain' => urlencode ( $referrer_domain ),
 					'wp_plugin_url' => urlencode (WP_PLUGIN_URL ),
-					'current_version' => urlencode ( $plugin_data['Version']),
+					'current_version' => urlencode ( $plugin_data['Version'] ),
 				],
-				$plugin_data['UpdateURI'] );
+				$update_uri );
 
 			$response = wp_remote_get(
-				esc_url($remote_get_url),
+				// FIXME: esc_url() broke url here.
+				$remote_get_url,
 				[
 					'timeout' => 10,
 					'headers' => [ 'Accept' => 'application/json', ]
@@ -66,12 +84,12 @@ class updater {
 	 * @param $action
 	 * @param $args
 	 *
-	 * @return void
+	 * @return \stdClass
 	 */
 	public static function plugin_modal_info ( $result, $action, $args ) {
 
 		// do nothing if this is not about getting plugin information
-		if ( $action !== 'plugin_information') {
+		if ( $action !== 'plugin_information' ) {
 			return $result;
 		}
 
